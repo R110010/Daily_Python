7c95f0d2b0f5fea68e0f5b0eaea1b70eb575ba77
R110010 <greatboyraj@gmail.com>
2025-03-24 18:11:13 +0530

    updated Day_15

--- Day_15/Ans_15.py
+++ Day_15/Ans_15.py
@@ -3,7 +3,7 @@ import requests
 import json
 url = "https://api.openweathermap.org/data/2.5/weather"
 
-api_key = "5bfa10f8ecc572532a95dd430d1ee5d0"
+api_key = "" # provide an API key.
 params = {
         'q': "Kalimpong",
         'appid': api_key,
